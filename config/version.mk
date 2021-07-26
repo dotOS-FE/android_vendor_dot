5c786a76987d4006548848095c0c3e61643ad99c
SevralT <a.chycherin@yahoo.com>
2021-07-26 08:25:11 +0000

    vendor: Bump version to 1.2

--- config/version.mk
+++ config/version.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 #dotOS Fan Edition Versioning :
-DOTFE_MOD_VERSION = v1.1
+DOTFE_MOD_VERSION = v1.2
 
 ifndef DOTFE_BUILD_TYPE
     DOTFE_BUILD_TYPE := Community
